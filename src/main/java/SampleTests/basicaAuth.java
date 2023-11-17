53d987ab4682ce4c422bf921c410f223fbd071bf
Srikanthgowda26 <srikanthgowda188@gmail.com>
2023-11-17 15:58:08 +0530

    Test commit 3

--- src/main/java/SampleTests/basicaAuth.java
+++ src/main/java/SampleTests/basicaAuth.java
@@ -11,7 +11,7 @@ import org.testng.Assert;
 public class basicaAuth {
 
 	public static void main(String[] args) {
-		// Initialize the ChromeDriver drive dri
+		// Initialize the ChromeDriver initializing
         WebDriver driver = new ChromeDriver();
 
         String urlWithAuth = "http://admin:admin@the-internet.herokuapp.com/basic_auth";
